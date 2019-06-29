c60b7a820bc54e5662efcc7b967917539a1af69f
LohitaPanapana <lohita59@yahoo.com>
2019-06-29 19:28:35 +0530

    Added completed functionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
 	var tasks = [];
+	tasks.push({
+		id: 1,
+		date: '27-5-2019',
+		task: 'task',
+		status: 'status'
+	});
+	showList(tasks);
 
 	$('#add').click(function(){
 		var id = 2;
@@ -8,17 +15,30 @@ $(document).ready(function() {
 		var task = $('input[type="text"]').val();
 		$('input[type="text"]').val("");
 		var status = "Pending";
-		var taskList = "<tr scope='row'><td>" + id + "</td><td>" + date + "</td><td>" + task + "</td><td>" + status +"</td></tr>";
-		$('tbody').append(taskList);
 		tasks.push({
 			id: id,
 			date: date,
 			task: task,
 			status: status
 		});
+		showList(tasks);
 		console.log(tasks);
 	});
 
-	
+	function showList(list){
+		$('tbody').html("");
 
+		list.forEach(function(item){
+			var taskList = "<tr scope='row'><td>" + item.id + "</td><td>" + item.date + "</td><td>" + item.task + "</td><td>" + item.status +"</td></tr>";
+			$('tbody').append(taskList);
+		})
+	}
+
+	$('tbody').on("click","tr", function(){
+		$(this).css("background-color", "rgba(0, 255, 0, 0.5)");
+		if($(this).html().indexOf("Pending") !== -1){
+			var data = $(this).html().replace("Pending", "Completed");
+			$(this).html(data);
+		}
+	})
 });
